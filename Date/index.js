8202f94aa59070471c1d5f6bc9fa22788d6e6a0c
Luis <machadosoftware@gmail.com>
2023-10-10 14:57:40 -0300

    up1

---
+++ Date/index.js
@@ -0,0 +1,11 @@
+
+// const tresHoras = 60 * 60 * 3 * 1000;
+// const umDia = 60 * 60 * 24 * 1000; 
+// const data = new Date();
+// const data = new Date(2019, 3, 20, 15, 14, 27);
+const data = new Date('2019-04-20 20:20:59')
+console.log('Dia', data.getDate())
+console.log('Mês', data.getMonth() + 1)
+console.log('Ano', data.getFullYear())
+console.log('Hora', data.getHours())
+console.log(data.toString())
---
+++ fizzBuzz/index.js
@@ -0,0 +1,12 @@
+function fizzBuzz (x) {
+  if (typeof x !== 'number') return x
+  if (x % 3 === 0 && x % 5 === 0) return 'FizzBuzz'
+  if (x % 3 === 0) return 'Fizz'
+  if (x % 5 === 0) return 'Buzz'
+  return x
+}
+
+console.log('a', fizzBuzz('a'))
+for (let i = 0; i <= 100; i++) {
+  console.log(i, fizzBuzz(i))
+}
