69b77a36cdfadbb0e3c3359bb1adfcd39fae944e
Luis <machadosoftware@gmail.com>
2023-10-11 01:46:52 -0300

    functions

---
+++ aula50/script.js
@@ -0,0 +1,23 @@
+// function conta(operador, acumulador, ...numeros) {
+//   for (let numero of numeros) {
+//     if (operador === '+') acumulador += numero
+//     if (operador === '-') acumulador -= numero
+//     if (operador === '/') acumulador /= numero
+//     if (operador === '*') acumulador *= numero
+//   }
+
+//   console.log(acumulador)
+// }
+// conta('+', 1, 20, 30, 40, 50)
+
+const conta = function(operador, acumulador, ...numeros) {
+  for (let numero of numeros) {
+    if (operador === '+') acumulador += numero
+    if (operador === '-') acumulador -= numero
+    if (operador === '/') acumulador /= numero
+    if (operador === '*') acumulador *= numero
+  }
+
+  console.log(acumulador)
+}
+conta('+', 1, 20, 30, 40, 50)
